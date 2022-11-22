9ee8db9ff7d57a96b3eacd3ec8b1066847c0aa7d
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-11-22 19:45:40 +0530

    Create a get message service

--- 009_StaticServingWithExpress/server.js
+++ 009_StaticServingWithExpress/server.js
@@ -1,7 +1,18 @@
+const { application } = require('express')
 var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: "Abhimanyu", message: "Hi"},
+    {name: "Jane", message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
 var server = app.listen(3000, () =>{
     console.log('server is listening to the port', server.address().port)
 })
