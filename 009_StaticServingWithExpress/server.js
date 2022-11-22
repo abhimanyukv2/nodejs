8cc686c5e64ddcbe86de8ddf657869a41ad7da3d
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-11-22 20:20:02 +0530

    Create a post message service

--- 009_StaticServingWithExpress/server.js
+++ 009_StaticServingWithExpress/server.js
@@ -1,8 +1,11 @@
-const { application } = require('express')
+// const { application } = require('express')
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name: "Abhimanyu", message: "Hi"},
@@ -11,6 +14,12 @@ var messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    // console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 var server = app.listen(3000, () =>{
